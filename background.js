1320268d2da5a96b2639037f362f4f148761d9ee
qunash <anzoria@gmail.com>
2023-01-05 12:15:30 +0400

    ~fix click on icon for manifest v2

--- background.js
+++ background.js
@@ -1,13 +1,22 @@
+var manifest_version = chrome.runtime.getManifest().manifest_version;
+
+function openChatGPTWebpage() {
+  chrome.tabs.create({
+    url: "https://chat.openai.com/chat",
+  });
+}
+
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({
-        num_web_results: 3,
-        web_access: true,
-    });
+  chrome.storage.sync.set({
+    num_web_results: 3,
+    web_access: true,
+  });
+  openChatGPTWebpage();
 });
 
-// open chatgpt website when clicking on the extension icon
-chrome.action.onClicked.addListener(tab => {
-    chrome.tabs.create({
-        url: "https://chat.openai.com/chat"
-    });
-});
+// open chatgpt webpage when extension icon is clicked
+if (manifest_version == 2) {
+  chrome.browserAction.onClicked.addListener(openChatGPTWebpage);
+} else {
+  chrome.action.onClicked.addListener(openChatGPTWebpage);
+}
