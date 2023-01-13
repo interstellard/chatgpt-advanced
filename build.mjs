1c7691feecf839f56ab5bb396356907e22d4268c
qunash <anzoria@gmail.com>
2023-01-13 15:23:21 +0300

    fixed zipping extensions in build script

--- build.mjs
+++ build.mjs
@@ -1,95 +1,100 @@
-import esbuild from "esbuild"
-import archiver from "archiver"
-import fs, { copy } from "fs-extra"
-import tailwindcss from "tailwindcss"
-import autoprefixer from "autoprefixer"
-import postcssPlugin from "esbuild-style-plugin"
-import copyStaticFilesPlugin from "esbuild-plugin-copy"
+import esbuild from "esbuild";
+import archiver from "archiver";
+import fs from "fs-extra";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import postcssPlugin from "esbuild-style-plugin";
+import copyStaticFilesPlugin from "esbuild-plugin-copy";
 
-const buildDir = "build"
+const buildDir = "build";
 
 async function deleteBuildDir() {
-    await fs.remove(buildDir)
-}   
-
-async function runEsbuild() {
-    await esbuild.build({
-        entryPoints: [
-            "src/content-scripts/cs.tsx",
-            "src/background/bg.ts",
-        ],
-        outdir: buildDir,
-        bundle: true,
-        minify: true,
-        treeShaking: true,
-        define: {
-            "process.env.NODE_ENV": '"production"',
-        },
-        jsxFactory: "h",
-        jsxFragment: "Fragment",
-        jsx: "automatic",
-        loader: {
-            ".png": "dataurl",
-        },
-        plugins: [
-            postcssPlugin({
-                postcss: {
-                    plugins: [tailwindcss, autoprefixer],
-                  },
-            }),
-            copyStaticFilesPlugin({
-                resolveFrom: 'cwd',
-                assets: {
-                    from: ['src/manifest.json','src/manifest.v2.json'],
-                    to: [buildDir, buildDir],
-                }
-            }),
-        ],
-    })
+  await fs.remove(buildDir);
 }
 
+async function runEsbuild() {
+  await esbuild.build({
+    entryPoints: ["src/content-scripts/cs.tsx", "src/background/bg.ts"],
+    outdir: buildDir,
+    bundle: true,
+    minify: true,
+    treeShaking: true,
+    define: {
+      "process.env.NODE_ENV": '"production"',
+    },
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
+    jsx: "automatic",
+    loader: {
+      ".png": "dataurl",
+    },
+    plugins: [
+      postcssPlugin({
+        postcss: {
+          plugins: [tailwindcss, autoprefixer],
+        },
+      }),
+      copyStaticFilesPlugin({
+        resolveFrom: "cwd",
+        assets: {
+          from: ["src/manifest.json"],
+          to: [buildDir],
+        },
+      }),
+    ],
+  });
+}
 
-async function zipForBrowser(browser) {
+async function zipExtensionForBrowser(browser) {
+  const manifest = await fs.readJson(`${buildDir}/manifest.json`);
+  const version = manifest.version;
+  let archiveName = `webchatgpt-${version}-${browser}.zip`;
 
-    const manifest = await fs.readJson(`${buildDir}/manifest.v2.json`)
-    const version = manifest.version;
-    let archiveName = `webchatgpt-${version}-${browser}.zip`;
+  const archive = archiver("zip", { zlib: { level: 9 } });
+  const stream = fs.createWriteStream(archiveName);
 
-    const tempDir = `${buildDir}/temp`;
-    await fs.ensureDir(tempDir);
+  archive.pipe(stream);
 
-    if(browser === 'firefox'){
-        await fs.rename(`${tempDir}/manifest.v2.json`, `${tempDir}/manifest.json`);
+  await addFilesToZip(archive, browser);
+
+  console.log(`Creating ${archiveName}…`);
+  archive.finalize();
+}
+
+async function addFilesToZip(archive, browser) {
+  const entries = await fs.readdir("build");
+  for (const entry of entries) {
+    const entryStat = await fs.stat(`build/${entry}`);
+
+    if (entryStat.isDirectory()) {
+      archive.directory(`build/${entry}`, entry);
+    } else {
+      if (entry === "manifest.json") continue;
+      archive.file(`build/${entry}`, { name: entry });
     }
-
-    const archive = archiver("zip", {
-        zlib: { level: 9 }
-    });
-    const stream = fs.createWriteStream(archiveName);
-
-    archive.pipe(stream);
-    archive.directory(tempDir, false);
-    if(browser === 'chrome'){
-        archive.glob("!manifest.v2.json");
-    }
-    await archive.finalize();
-    await fs.remove(tempDir);
+  }
+  if (browser === "firefox") {
+    archive.file("src/manifest.v2.json", { name: "manifest.json" });
+  } else if (browser === "chrome") {
+    archive.file("build/manifest.json", { name: "manifest.json" });
+  }
 }
 
 async function build() {
-    
-    await deleteBuildDir()
-    await runEsbuild()
-    
-    const createZips = process.argv.includes('--create-zips');
-    if(createZips){
-        try {
-            await zipForBrowser("chrome");
-            await zipForBrowser("firefox");
-        } catch (error) {
-            console.error(error);
-        }
+  await deleteBuildDir();
+  await runEsbuild();
+
+  const createZips = process.argv.includes("--create-zips");
+  if (createZips) {
+    try {
+      await zipExtensionForBrowser("chrome");
+      await zipExtensionForBrowser("firefox");
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  console.log("Build complete");
 }
 
-build()
+build();
