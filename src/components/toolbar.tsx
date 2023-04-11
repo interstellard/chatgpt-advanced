bb937d0ae9a8b44ab6d0df96349cc39b9ad0f063
Kenny Johnson <105308787+papercircuit@users.noreply.github.com>
2023-04-11 11:05:26 -0400

    Fix centering issue on Web Access Toggle
    I noticed the selector inside the webAccessToggle element was not being centered correctly on screens wider than 766px and less than 1280px. I fixed this using top: 50% and transform: translateY(-50%).

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -118,15 +118,15 @@ function Toolbar(
 
 
     const webAccessToggle =
-        <div className="wcg-group wcg-relative wcg-flex">
-            <label className="wcg-relative wcg-inline-flex wcg-cursor-pointer wcg-items-center">
-                <input type="checkbox" value="" className="wcg-peer wcg-sr-only" checked={webAccess} onChange={handleWebAccessToggle} title="Web access" />
-                <div className="dark:wcg-peer-focus:ring-blue-800 wcg-peer wcg-h-5 wcg-w-9 wcg-rounded-full wcg-bg-gray-500 after:wcg-absolute after:wcg-top-[2px] after:wcg-left-[2px] after:wcg-h-4 after:wcg-w-4 after:wcg-rounded-full after:wcg-border after:wcg-border-gray-300 after:wcg-bg-white after:wcg-transition-all after:wcg-content-[''] peer-checked:wcg-bg-emerald-700 peer-checked:after:wcg-translate-x-full peer-checked:after:wcg-border-white peer-focus:wcg-ring-2 peer-focus:wcg-ring-white dark:wcg-border-gray-600" />
-                <span className="wcg-ml-1 wcg-pl-1 wcg-text-sm wcg-font-semibold after:wcg-content-['Web'] md:after:wcg-content-['Web_access']" />
-            </label>
-            <span class="wcg-absolute wcg-left-1/2 wcg-m-4 wcg-mx-auto -wcg-translate-x-6 wcg-translate-y-3 wcg-rounded-md wcg-bg-gray-800 wcg-p-1 
-            wcg-text-xs wcg-text-gray-100 wcg-opacity-0 wcg-transition-opacity group-hover:wcg-opacity-100">Alt+W</span>
-        </div>
+    <div className="wcg-group wcg-relative wcg-flex">
+        <label className="wcg-relative wcg-inline-flex wcg-cursor-pointer wcg-items-center">
+            <input type="checkbox" value="" className="wcg-peer wcg-sr-only" checked={webAccess} onChange={handleWebAccessToggle} title="Web access" />
+            <div className="dark:wcg-peer-focus:ring-blue-800 wcg-peer wcg-h-5 wcg-w-9 wcg-rounded-full wcg-bg-gray-500 after:wcg-absolute after:wcg-top-1/2 after:wcg-left-[2px] after:wcg--translate-y-1/2 after:wcg-h-4 after:wcg-w-4 after:wcg-rounded-full after:wcg-border after:wcg-border-gray-300 after:wcg-bg-white after:wcg-transition-all after:wcg-content-[''] peer-checked:wcg-bg-emerald-700 peer-checked:after:wcg-translate-x-full peer-checked:after:wcg-border-white peer-focus:wcg-ring-2 peer-focus:wcg-ring-white dark:wcg-border-gray-600" />
+            <span className="wcg-ml-1 wcg-pl-1 wcg-text-sm wcg-font-semibold after:wcg-content-['Web'] md:after:wcg-content-['Web_access']" />
+        </label>
+        <span class="wcg-absolute wcg-left-1/2 wcg-m-4 wcg-mx-auto -wcg-translate-x-6 wcg-translate-y-3 wcg-rounded-md wcg-bg-gray-800 wcg-p-1 
+        wcg-text-xs wcg-text-gray-100 wcg-opacity-0 wcg-transition-opacity group-hover:wcg-opacity-100">Alt+W</span>
+    </div>
 
     return (
         <div className="wcg-flex wcg-flex-col wcg-gap-0">
