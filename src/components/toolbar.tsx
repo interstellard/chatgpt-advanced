3d78373fcf8a65d682a52585dadb8626a39d1020
qunash <anzoria@gmail.com>
2023-04-11 19:52:02 +0300

    ~fix UI in light mode; make options btn focusable

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -121,7 +121,7 @@ function Toolbar(
         <div className="wcg-group wcg-relative wcg-flex">
             <label className="wcg-relative wcg-inline-flex wcg-cursor-pointer wcg-items-center">
                 <input type="checkbox" value="" className="wcg-peer wcg-sr-only" checked={webAccess} onChange={handleWebAccessToggle} title="Web access" />
-                <div className="dark:wcg-peer-focus:ring-blue-800 wcg-peer wcg-h-5 wcg-w-9 wcg-rounded-full wcg-bg-gray-500 after:wcg-absolute after:wcg-top-[2px] after:wcg-left-[2px] after:wcg-h-4 after:wcg-w-4 after:wcg-rounded-full after:wcg-border after:wcg-border-gray-300 after:wcg-bg-white after:wcg-transition-all after:wcg-content-[''] peer-checked:wcg-bg-emerald-700 peer-checked:after:wcg-translate-x-full peer-checked:after:wcg-border-white peer-focus:wcg-ring-2 peer-focus:wcg-ring-white dark:wcg-border-gray-600" />
+                <div className="wcg-peer wcg-h-5 wcg-w-9 wcg-rounded-full wcg-bg-gray-500 after:wcg-absolute after:wcg-top-[2px] after:wcg-left-[2px] after:wcg-h-4 after:wcg-w-4 after:wcg-rounded-full after:wcg-border after:wcg-border-gray-300 after:wcg-bg-white after:wcg-transition-all after:wcg-content-[''] peer-checked:wcg-bg-emerald-700 peer-checked:after:wcg-translate-x-full peer-checked:after:wcg-border-white peer-focus:wcg-ring-2 peer-focus:wcg-ring-white dark:wcg-border-gray-600" />
                 <span className="wcg-ml-1 wcg-pl-1 wcg-text-sm wcg-font-semibold after:wcg-content-['Web'] md:after:wcg-content-['Web_access']" />
             </label>
             <span class="wcg-absolute wcg-left-1/2 wcg-m-4 wcg-mx-auto -wcg-translate-x-6 wcg-translate-y-3 wcg-rounded-md wcg-bg-gray-800 wcg-p-1 
@@ -131,7 +131,8 @@ function Toolbar(
     return (
         <div className="wcg-flex wcg-flex-col wcg-gap-0">
             <div className="wcg-toolbar wcg-flex wcg-items-center wcg-gap-2 wcg-rounded-md wcg-px-1">
-                <div className="wcg-btn-xs wcg-btn"
+                <div className="wcg-btn-xs wcg-btn focus:wcg-ring-2 focus:wcg-ring-white"
+                    tabIndex={0}
                     onClick={() => Browser.runtime.sendMessage("show_options")}
                 >
                     {icons.tune}
